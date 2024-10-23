d2493fb6b5f837cb7b25ca9883cd8df604e3e3c4
11saran <1211saran@gmail.com>
2024-10-23 15:21:16 +0530

    Const Variables

---
+++ Const.js
@@ -0,0 +1,13 @@
+//Object
+const person ={
+    name : "Ravi",
+    age:24
+}
+
+person.name = "Kumar"
+console.log(person)
+
+//Add new value in array using push 
+const fruits = ["Apple","Mango"]   
+fruits.push("Banana")
+console.log(fruits)
